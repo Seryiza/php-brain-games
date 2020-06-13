1772e5868da70cae6cf6d6dfd80116dfc89c9b38
Sergey Zaborovsky <seryiza@yandex.ru>
2020-06-13 16:52:25 +1100

    Рефакторинг: вынесение строки с правилами игры в константу

--- src/games/even.php
+++ src/games/even.php
@@ -2,6 +2,8 @@
 
 namespace Seryiza\BrainGames\Games;
 
+const EVEN_GAME_RULES = 'Answer "yes" if number even otherwise answer "no".';
+
 function isEven(int $number)
 {
     return $number % 2 === 0;
@@ -9,13 +11,11 @@ function isEven(int $number)
 
 function getEvenGame(): array
 {
-    $rules = 'Answer "yes" if number even otherwise answer "no".';
-
     $game = function () {
         $number = rand(1, 100);
         $answer = isEven($number) ? 'yes' : 'no';
         return [$number, $answer];
     };
 
-    return [$rules, $game];
+    return [EVEN_GAME_RULES, $game];
 }

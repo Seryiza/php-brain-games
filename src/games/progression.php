f9c4dadb4192180b933957d40c300f68e1b65aca
Sergey Zaborovsky <seryiza@yandex.ru>
2020-06-13 17:40:21 +1100

    Добавлена игра с отгадыванием числа в прогрессии

---
+++ src/games/progression.php
@@ -0,0 +1,40 @@
+<?php
+
+namespace Seryiza\BrainGames\Games\ProgressionGame;
+
+const NUMBERS_IN_SEQUENCE = 8;
+const RULES = 'What number is missing in this progression?';
+
+function getNumberSequence(int $startNumber, int $difference, int $sequenceSize): array
+{
+    $numbers = [];
+    for ($i = 0; $i < $sequenceSize; $i++) {
+        $numbers[$i] = $startNumber + $i * $difference;
+    }
+    return $numbers;
+}
+
+function formatNumbersWithHidedOne(array $numbers, int $hidingNumberIndex): string
+{
+    $numbers[$hidingNumberIndex] = '..';
+    return implode(' ', $numbers);
+}
+
+function getProgressionGame(): array
+{
+    $game = function () {
+        $startNumber = rand(1, 80);
+        $difference = rand(1, 10);
+        $numberSequence = getNumberSequence($startNumber, $difference, NUMBERS_IN_SEQUENCE);
+
+        $hidedNumberIndex = rand(1, NUMBERS_IN_SEQUENCE - 2);
+        $hidedNumber = $numberSequence[$hidedNumberIndex];
+
+        $answer = (string) $hidedNumber;
+        $question = formatNumbersWithHidedOne($numberSequence, $hidedNumberIndex);
+
+        return [$question, $answer];
+    };
+
+    return [RULES, $game];
+}

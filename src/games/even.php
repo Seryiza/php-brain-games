cabf9a12c61a3aa125b140d6fab67e2c816dad6d
Sergey Zaborovsky <seryiza@yandex.ru>
2020-06-13 16:34:36 +1100

    Добавлена игра brain-even и первая идея движка игры

---
+++ src/games/even.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace Seryiza\BrainGames\Games;
+
+use function cli\line;
+use function cli\prompt;
+
+function isEven(int $number) {
+    return $number % 2 === 0;
+}
+
+function getEvenGame(): callable
+{
+    return function () {
+        $number = rand(1, 100);
+        $correctAnswer = isEven($number) ? 'yes' : 'no';
+
+        line("Question: {$number}");
+        $enteredAnswer = prompt('Your answer');
+
+        $isCorrect = $enteredAnswer === $correctAnswer;
+        if ($isCorrect) {
+            line('Correct!');
+        } else {
+            line("'{$enteredAnswer}' is wrong answer :( Correct answer was '{$correctAnswer}'.");
+        }
+
+        return $isCorrect;
+    };
+}
